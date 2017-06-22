ebcb8e8c44c3e3ed6e0258f0dde4b4acca2043e2
zp <intheair100@163.com>
2017-06-22 17:04:56 +0800

    封装返回给客户端的逻辑

--- server/helper/common.php
+++ server/helper/common.php
@@ -5,7 +5,23 @@
  * Date: 17/6/22
  * Time: 下午2:17
  */
-function returnData($msg,$code,$data){
 
+//返回给客户端的数据
+function returnData($msg,$code,$data){
+    $result['msg'] = $msg;
+    $result['code'] = $code;
+    if ($data){
+        $result['data'] = $data;
+    }
+    $json = JSON($result);
+    return $json;
+}
+
+//返回msg信息
+function returnMsg($msg, $code){
+    $result['msg'] = $msg;
+    $result['code'] = $code;
+    $json = JSON($result);
+    return $json;
 }
 ?>
